f883a86d68dc74b33b29e09ca3237623d2714fe5
KianMun <kianmun80@yahoo.com>
2024-01-30 09:10:26 +0800

    Updated flask server to handle no input of symptoms

--- diseasePredictionServer.py
+++ diseasePredictionServer.py
@@ -15,10 +15,14 @@ model = load_model('RFPredictDiesease.pkl')
 @app.route("/predict", methods=["GET"])
 def predict():
     data = request.get_json()
-    npArray = np.array(data)
-    reshaped_data = npArray.reshape(1,-1)
-    prediction = model.predict(reshaped_data)
-    return jsonify({"prediction": prediction[0]})
+    #Check for no input
+    if(all(element == 0 for element in data)):
+        return jsonify({"message": "No symptoms were selected!"})
+    else:
+        npArray = np.array(data)
+        reshaped_data = npArray.reshape(1,-1)
+        prediction = model.predict(reshaped_data)
+        return jsonify({"prediction": prediction[0]})
 
 if __name__ == "__main__":
     app.run(debug=True)
